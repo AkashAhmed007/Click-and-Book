d23514c60f5b6f4c5c24536cab6a6fd877e3d98e
Akash Ahmed <akashahmedlikhon.007@gmail.com>
2024-10-08 18:45:13 +0600

    cancel a room and status set route created

--- server/index.js
+++ server/index.js
@@ -205,6 +205,13 @@ async function run() {
       res.send(result)
     })
 
+    app.delete("/booking/:id",verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.post("/jwt", async (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
